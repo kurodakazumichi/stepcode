6c82097a6c94b30314a4303bb880adc96b149092
黒田一道 <>
2019-04-01 14:21:48 +0900

    updated 日本語対応

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,12 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    { languages: true },
     {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
+    
   ],
 
   // If you have users set above, you add it here:
