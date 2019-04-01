f51d412d214122d89144d57626e1923ff47e7919
黒田一道 <>
2019-04-02 02:22:49 +0900

    Updated index.ts

--- packages/stepcode-core/src/index.ts
+++ packages/stepcode-core/src/index.ts
@@ -1,3 +1,14 @@
+/**
+ * stepcode-coreのエントリーポイント
+ */
 import StepCode from './StepCode';
+
+/**
+ * 詳細は[[StepCode]] classをみてください。
+ */
 export default StepCode;
+
+/**
+ * 詳細は[[Step]] classをみてください。
+ */
 export { default as Step } from './Step';
