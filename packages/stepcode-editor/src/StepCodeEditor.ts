8598be7420697201318e2db1ca9ddc9f8ff7ce9e
黒田一道 <>
2019-04-03 14:04:52 +0900

    Updated UIの構築をUI.tsで管理するように

--- packages/stepcode-editor/src/StepCodeEditor.ts
+++ packages/stepcode-editor/src/StepCodeEditor.ts
@@ -3,77 +3,34 @@
  *****************************************************************************/
 import Core from 'stepcode-core';
 import StepCode from 'stepcode';
-import * as Config from './config';
 import Ace from 'ace-builds';
 import ThemeGithub from 'ace-builds/src-noconflict/theme-github';
+import UI from './UI';
+
 
 /******************************************************************************
  * StepCodeEditorの本体
  *****************************************************************************/
 export default class StepCodeEditor {
 
-  constructor(selector:string | HTMLElement) 
+  constructor(target:string | HTMLElement) 
   {
     // StepCode(コア)を保持
     this.core = new Core({});
 
-    // ルート要素を取得、保持
-    this.root = this.getRoot(selector);
-
     console.log(this.core);
-    this.buildElement();
-  }
+    //this.buildElement();
 
-  private buildElement() {
+    this.ui = new UI(target);
 
-    const main = Config.createElement(Config.UIType.Main);
-    const mainEditor = Config.createElement(Config.UIType.MainEditor);
-    const mainPreview = Config.createElement(Config.UIType.MainPreview);
-    const mainPreviewStepCode = Config.createElement(Config.UIType.MainPreviewStepCode);
-    const editorTitle = Config.createElement(Config.UIType.EditorTitle);
-    const editorTitleText = Config.createElement(Config.UIType.EditorTitleText);
-    const editorCode = Config.createElement(Config.UIType.EditorCode);
-    const editorCodeAce = Config.createElement(Config.UIType.EditorCodeAce);
-    const editorMd = Config.createElement(Config.UIType.EditorMd);
-    const editorMdInput = Config.createElement(Config.UIType.EditorMdInput);
-    const editorFooter = Config.createElement(Config.UIType.EditorFooter);
-    const editorFooterLogo = Config.createElement(Config.UIType.EditorFooterLogo);
-    const menu = Config.createElement(Config.UIType.Menu);
-    const menuSaveButton = Config.createElement(Config.UIType.MenuSaveButton);
-
-    // メインの構築
-    main.appendChild(mainEditor);
-    main.appendChild(mainPreview);
+    this.stepcode = new StepCode(this.ui.stepcode, {});
     
+    this.ace = Ace.edit(this.ui.ace);
+    this.ace.getSession().setUseWorker(false);
+    this.ace.setTheme(ThemeGithub);
 
-    // エディターの構築
-    mainEditor.appendChild(editorTitle);
-    editorTitle.appendChild(editorTitleText);
-    mainEditor.appendChild(editorCode);
-    editorCode.appendChild(editorCodeAce);
-    mainEditor.appendChild(editorMd);
-    editorMd.appendChild(editorMdInput);
-    mainEditor.appendChild(editorFooter);
-    editorFooter.appendChild(editorFooterLogo);
-
-    // プレビューの構築
-    mainPreview.appendChild(mainPreviewStepCode);
-
-    // メニューの構築
-    menu.appendChild(menuSaveButton);
-
-    // ルートの構築
-    this.root.appendChild(main);
-    this.root.appendChild(menu);
-
-    // ステップコード初期化
-    new StepCode(mainPreviewStepCode, {});
-
-    // Aceを初期化
-    const editor = Ace.edit(editorCodeAce);
-    editor.getSession().setUseWorker(false);
-    editor.setTheme(ThemeGithub);
-
+    // コンパイルエラー対策
+    console.log(this.stepcode);
   }
 
   //---------------------------------------------------------------------------
@@ -82,30 +39,19 @@ export default class StepCodeEditor {
   /** StepCode本体 */
   private core:Core;
 
-  /** UI Root */
-  private root:HTMLElement;
+  /** 全てのHTMLELementをもつUIインスタンス */
+  private ui:UI;
+
+  /** StepCode本体 */
+  private stepcode:StepCode;
+
+  /** Ace Editor */
+  private ace: Ace.Ace.Editor;
 
   //---------------------------------------------------------------------------
   // private メソッド
 
-  /**
-   * ルート要素を取得する。
-   * @param target ルート要素を取得するselector、もしくはルート要素
-   */
-  private getRoot(target:string | HTMLElement) : HTMLElement 
-  {
-    let root;
 
-    // targetがHTMLElementであればそのまま
-    if (target instanceof HTMLElement) {
-      root = target;
-    } 
-    
-    // HTMLElementでなければ、selector文字列として処理する
-    else {
-      root = document.querySelector(target) as HTMLElement;
-    }
 
-    return root;
-  }
+
 }
---
+++ packages/stepcode-editor/src/UI.ts
@@ -0,0 +1,157 @@
+/******************************************************************************
+ * import
+ *****************************************************************************/
+import * as Config from './Config';
+
+/******************************************************************************
+ * StepCodeで必要な全てのHTMLElementを管理するクラス
+ *****************************************************************************/
+export default class UI {
+
+  /**
+   * StepCodeEditorに必要な全てのHTMLElementを生成・構築する
+   * @param target ルート要素を取得するセレクター、またはHTML要素
+   */
+  constructor(target:string | HTMLElement) 
+  {
+    // ルート要素を取得、保持
+    this.root = this.getRoot(target);
+
+    // 各種要素を作成・構築
+    this.doms = {};
+    this.buildElements();
+  }
+
+  //---------------------------------------------------------------------------
+  // private プロパティ
+
+  /** UI Root */
+  private root:HTMLElement;
+
+  /** StepCodeEditorの全HTMLElementリスト */
+  private doms:{[key:string]:HTMLElement};
+
+  //---------------------------------------------------------------------------
+  // private プロパティ
+
+  /** StepCodeに該当するHTMLElementを返します */
+  get stepcode():HTMLElement {
+    return this.doms[Config.UIType.MainPreviewStepCode];
+  }
+
+  /** AceEditorに該当するHTMLElementを返します */
+  get ace():HTMLElement {
+    return this.doms[Config.UIType.EditorCodeAce];
+  }
+
+  //---------------------------------------------------------------------------
+  // private メソッド
+
+  /**
+   * ルート要素を取得する。
+   * @param target ルート要素を取得するselector、もしくはルート要素
+   */
+  private getRoot(target:string | HTMLElement) : HTMLElement 
+  {
+    let root;
+
+    // targetがHTMLElementであればそのまま
+    if (target instanceof HTMLElement) {
+      root = target;
+    } 
+    
+    // HTMLElementでなければ、selector文字列として処理する
+    else {
+      root = document.querySelector(target) as HTMLElement;
+    }
+
+    return root;
+  }
+
+  /**
+   * HTMLElementを生成し、階層構造を構築する
+   */
+  private buildElements() {
+    this.createHTMLElements();
+    this.buildMainElements();
+    this.buildEditorElements();
+    this.buildPreviewElements();
+    this.buildMenuElements();
+
+    // ルート要素の階層構築
+    this.root.appendChild(this.doms[Config.UIType.Main]);
+    this.root.appendChild(this.doms[Config.UIType.Menu]);
+  }
+
+  /** 
+   * 全てのHTMLElementを生成する 
+   */
+  private createHTMLElements() 
+  {
+    Object.values(Config.UIType).map((uiKey) => {
+      this.doms[uiKey] = Config.createElement(uiKey as Config.UIType);
+    })
+  }
+
+  /**
+   *  メイン要素の階層構造を構築する 
+   */
+  private buildMainElements() 
+  {
+    const dom = this.doms;
+    const ui = Config.UIType;
+   
+    dom[ui.Main].appendChild(dom[ui.MainEditor]);
+    dom[ui.Main].appendChild(dom[ui.MainPreview]);
+  }
+
+  /** 
+   * エディター要素の階層構造を構築する 
+   */
+  private buildEditorElements() 
+  {
+    const dom = this.doms;
+    const ui = Config.UIType;
+
+    // エディタ直下
+    dom[ui.MainEditor].appendChild(dom[ui.EditorTitle]);
+    dom[ui.MainEditor].appendChild(dom[ui.EditorCode]);
+    dom[ui.MainEditor].appendChild(dom[ui.EditorMd]);
+    dom[ui.MainEditor].appendChild(dom[ui.EditorFooter]);
+
+    // エディタ:タイトル直下
+    dom[ui.EditorTitle].appendChild(dom[ui.EditorTitleText]);
+
+    // エディタ:コード直下
+    dom[ui.EditorCode].appendChild(dom[ui.EditorCodeAce]);
+    
+    // エディタ:マークダウン直下
+    dom[ui.EditorMd].appendChild(dom[ui.EditorMdInput]);
+    
+    // エディタ:フッター直下
+    dom[ui.EditorFooter].appendChild(dom[ui.EditorFooterLogo]);
+  }
+
+  /** 
+   * プレビュー要素の階層構造を構築 
+   */
+  private buildPreviewElements() 
+  {
+    const dom = this.doms;
+    const ui = Config.UIType;
+
+    dom[ui.MainPreview].appendChild(dom[ui.MainPreviewStepCode]);
+  }
+
+  /** 
+   * メニュー要素の階層構造を構築 
+   */
+  private buildMenuElements() 
+  {
+    const dom = this.doms;
+    const ui = Config.UIType;
+
+    dom[ui.Menu].appendChild(dom[ui.MenuSaveButton]);
+  }
+
+}
