5796c050dd2d3b18deed656081a90cb7d0713430
黒田一道 <>
2019-04-03 12:33:27 +0900

    Update examples

--- examples/src/editor.ts
+++ examples/src/editor.ts
@@ -1,1 +1,11 @@
-console.log("editor demo");
+import StepCodeEditor from 'stepcode-editor';
+import 'stepcode/styles/style.scss';
+import 'stepcode-editor/styles/style.scss';
+
+/**
+ * StepCodeEditorを初期化
+ */
+new StepCodeEditor(document.querySelector('.sce-root') as HTMLElement);
+
+
+
