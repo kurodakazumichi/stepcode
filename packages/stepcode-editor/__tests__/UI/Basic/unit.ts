d3cfbee3b990b8f0e73d725eb3acfc35cedbe70d
黒田一道 <>
2019-04-10 21:34:57 +0900

    Added テスト

---
+++ packages/stepcode-editor/__tests__/UI/Basic/unit.ts
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * import
+ *****************************************************************************/
+import * as Dom from '../../__tools__/dom';
+import * as UI from '../../../src/UI';
+import StepCode from 'stepcode';
+
+describe('UI::生成に関する検証', () => {
+
+  describe(`selectorが指定された場合`, () => {
+    Dom.init();
+    const ui = new UI.default(Dom.AppSelectorString);
+
+    it('Snapshot', () => {
+      expect(ui).toMatchSnapshot();
+    })
+    
+  })
+
+  describe(`HTMLElementが指定された場合`, () => {
+    Dom.init();
+    const ui = new UI.default(Dom.appElement());
+
+    it('Snapshot', () => {
+      expect(ui).toMatchSnapshot();
+    })
+  })
+
+});
+
+describe('UI::公開プロパティの検証', () => {
+
+  Dom.init();
+  const ui = new UI.default(Dom.AppSelectorString);
+
+  it('stepcodeがStepCodeのインスタンスであること', () => {
+    expect(ui.stepcode).toBeInstanceOf(StepCode);
+  });
+
+})
