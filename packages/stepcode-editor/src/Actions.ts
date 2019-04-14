a81d38851daefa71da264fe471714be314d3cf76
黒田一道 <>
2019-04-15 03:26:15 +0900

    Clean

--- packages/stepcode-editor/src/Actions.ts
+++ packages/stepcode-editor/src/Actions.ts
@@ -1,29 +1,55 @@
+/******************************************************************************
+ * import
+ *****************************************************************************/
 import * as Util from '@puyan/stepcode-util';
+import { Step } from 'stepcode-core';
+import * as Config from './Config';
 import Store from './Store';
 import * as UI from './UI';
-import * as Config from './Config';
-import { Step } from 'stepcode-core';
 
+/** ステップを追加する位置、指定できる候補を定義 */
 type PositionToAddStep = 'last' | 'before' | 'after';
 
+/******************************************************************************
+ * Actionクラス
+ * StepCodeEditorで行えるアクションを定義する。
+ *****************************************************************************/
 export default class Actions {
+  /** コンストラクタ */
   constructor(store: Store, ui: UI.default) {
     this.store = store;
     this.ui = ui;
   }
 
+  /** プロパティ */
   private store: Store;
   private ui: UI.default;
 
+  //---------------------------------------------------------------------------
+  // 基本アクション
+
+  /** 初期データでリセットする */
   public reset() {
     this.load(Config.INIT_DATA);
   }
 
+  /** ロード */
   public load(data: any) {
     this.store.load(data);
     this.ui.update(this.store);
   }
 
+  /**
+   * データをダウンロードする。
+   * 引数が指定された場合は入力中の内容がダウンロードされる。
+   */
+  public download(title?: string, json?: any) {
+    title = title ? title : this.store.mainTitle;
+    json = json ? json : this.store.core.json;
+    Util.file.download(title, json);
+  }
+
+  /** プレビュー */
   public preview(data: {
     title?: string;
     file?: string;
@@ -35,14 +61,39 @@ export default class Actions {
     this.ui.stepcode.preview(this.store.getCurrentStep());
   }
 
+  /** 指定したidxを表示(UI全体を更新) */
+  public show(idx: number) {
+    this.store.atStep(idx);
+    this.ui.update(this.store);
+  }
+
+  /** １つ前を表示(UI全体を更新) */
+  public toPrev() {
+    this.store.toPrevStep();
+    this.ui.update(this.store);
+  }
+
+  /** １つ次を表示(UI全体を更新) */
+  public toNext() {
+    this.store.toNextStep();
+    this.ui.update(this.store);
+  }
+
+  //---------------------------------------------------------------------------
+  // スクロール位置の同期
+
   public syncScrollTopEditorToPreview() {
     const value = this.ui.ace.getSession().getScrollTop();
     this.ui.stepcode.setScrollTopToEditor(value);
   }
+
   public syncScrollTopPreviewToEditor() {
     const value = this.ui.stepcode.getScrollTopOfEditor();
     this.ui.ace.getSession().setScrollTop(value);
   }
+
+  //---------------------------------------------------------------------------
+  // ステップの追加、削除、移動
 
   public addStep(position: PositionToAddStep, step?: Step) {
     let guideIndex;
@@ -77,26 +128,8 @@ export default class Actions {
     this.ui.update(this.store);
   }
 
-  public download(title?: string, json?: any) {
-    title = title ? title : this.store.mainTitle;
-    json = json ? json : this.store.core.json;
-    Util.file.download(title, json);
-  }
-
-  public show(idx: number) {
-    this.store.atStep(idx);
-    this.ui.update(this.store);
-  }
-
-  public toPrev() {
-    this.store.toPrevStep();
-    this.ui.update(this.store);
-  }
-
-  public toNext() {
-    this.store.toNextStep();
-    this.ui.update(this.store);
-  }
+  //---------------------------------------------------------------------------
+  // プレビューの表示位置操作
 
   showPreview(idx: number) {
     this.ui.stepcode.show(idx);
@@ -109,6 +142,9 @@ export default class Actions {
   toNextPreview() {
     this.ui.stepcode.next();
   }
+
+  //---------------------------------------------------------------------------
+  // エディターの表示位置操作
 
   public showEditor(idx: number) {
     this.store.atStep(idx);
@@ -125,10 +161,16 @@ export default class Actions {
     this.ui.updateEditor(this.store);
   }
 
+  //---------------------------------------------------------------------------
+  // ガイドの操作
+
   public selectGuide(idx: number) {
     this.ui.resetGuideItemClassAll();
     this.ui.modifyGuideItemToSelected(idx);
   }
+
+  //---------------------------------------------------------------------------
+  // 各種同期
 
   public syncStoreToPreview() {
     this.ui.updateStepCode(this.store);
--- packages/stepcode-editor/src/StepCodeEditor.ts
+++ packages/stepcode-editor/src/StepCodeEditor.ts
@@ -7,6 +7,7 @@ import * as Config from './Config';
 import Store from './Store';
 import Actions from './Actions';
 import Events from './Events';
+
 /******************************************************************************
  * StepCodeEditorの本体
  *****************************************************************************/
